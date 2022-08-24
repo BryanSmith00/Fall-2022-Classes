05140f10c4d4c00e21143be08a4bec5ffd897ec0
Bryan Smith <46660845+BryanSmith00@users.noreply.github.com>
2022-08-24 00:58:52 -0500

    3360 programming assignment 1

---
+++ 3360 Operating Systems/programming-assignments/programming-assignment-1/main.cpp
@@ -0,0 +1,13 @@
+// Write your program here
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string text;
+    getline(cin, text);
+    cout << "the text is " << text << endl;
+
+    return 0;
+}
