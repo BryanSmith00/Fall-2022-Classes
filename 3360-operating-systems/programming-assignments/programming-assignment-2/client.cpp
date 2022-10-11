8420a77c2c3554663736285611345095028b465c
bryansmith00 <bryansmith8023@gmail.com>
2022-10-11 12:58:51 -0500

    monday

--- 3360-operating-systems/programming-assignments/programming-assignment-2/client.cpp
+++ 3360-operating-systems/programming-assignments/programming-assignment-2/client.cpp
@@ -1,42 +1,60 @@
 #include <unistd.h>
-#include <iostream>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_map>
 
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
-
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
     char buffer[256];
 
     if (argc < 3)
     {
         std::cerr << "usage " << argv[0] << "hostname port\n";
-        exit(0);
+        exit(1);
     }
 
     portno = atoi(argv[2]);
+
+    // Get the symbol from cin as a line then process it and store it before sending to the server
+    std::string message;
+    std::getline(std::cin, message);
+
+    std::unordered_map<char, int> symbols; 
+
+    // Adds each of the symbols to an unordered_map with its number of occurances
+    for (int i = 0; i < message.length(); i++) { 
+        symbols[message[i]]++; 
+    }
+
+    // For each of the child processes to be able to store its value in a common memory location we'll likely need pipes
+    
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0)
+    {
         std::cerr << "ERROR opening socket";
+        exit(1);
+    }
 
     server = gethostbyname(argv[1]);
     if (server == NULL)
     {
         std::cerr << "ERROR, no such host\n";
-        exit(0);
+        exit(1);
     }
 
     bzero((char *)&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_family = AF_INET; // internet family of protocols
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
 
@@ -47,15 +65,8 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    std::cout << "Please enter the message: ";
-
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
-
-    char message[] = "Bryan Smith";
-    int sMessage = strlen(buffer);
-
-    n = write(sockfd, &sMessage, strlen(buffer));
+    // sends data to the socket sockfd in the form of n number of bytes
+    n = write(sockfd, &message, message.length());
 
     if (n < 0)
     {
---
+++ 4353-software-design/notes/week07/2-creating-evolutionary-design-and-architecture-partii.md
@@ -0,0 +1,1 @@
+# 4353 Software Design Week 07 Videos Part 2 Creating Evolutionary Design and Architecture: Part II
