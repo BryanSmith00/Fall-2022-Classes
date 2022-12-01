108a294f15d6aac01fb0f0dd8288e1b77faa9469
bryansmith00 <bryansmith8023@gmail.com>
2022-12-01 14:38:05 -0600

    everything

--- 3360-operating-systems/programming-assignments/programming-assignment-3/main.cpp
+++ 3360-operating-systems/programming-assignments/programming-assignment-3/main.cpp
@@ -6,22 +6,33 @@
 #include <map>
 
 struct arg_struct {
-    std::string* code;
-    double* cdf;
-    double *prob;
+    char *character;
+    int *thread_num;
+    std::vector<double> *prob;
+    pthread_mutex_t *semB;
+    pthread_mutex_t *semP;
+    pthread_cond_t *print;
+    int *turn;
 };
 
 void* shannon(void* arguments);
 
 int main(int argc, char* argv[]) {
 
+    static pthread_mutex_t semB;
+    static pthread_mutex_t semP;
+    pthread_mutex_init(&semB, NULL);
+    pthread_mutex_init(&semP, NULL);
+
+    static pthread_cond_t print = PTHREAD_COND_INITIALIZER;
+
     std::string message;
     std::string prob;
     std::string symb;
+    std::vector<char> characters{};
     std::map<char, int> symbols; 
     std::vector<double> probabilities{};
-    std::vector<double> cdf{};
-    std::vector<std::string> codes{};
+    int turn = 0;
 
     // gets the input line from standard in 
     std::getline(std::cin, message);
@@ -29,45 +40,41 @@ int main(int argc, char* argv[]) {
     // Adds each of the symbols to an unordered_map with its number of occurances
     for (int i = 0; i < message.length(); i++) { 
         symbols[message[i]]++; 
+        characters.push_back(message[i]);
     }
 
     // calculates the probability for each symbol and stores it in a vector
     for (auto const& [key, val] : symbols)
         probabilities.push_back((double)val / message.length());
 
-    // calculates the modified cdf value for each symbol and adds it to a vector
-    for (int i = 0; i < probabilities.size(); i++)
-    {
-        double tempCdf = 0.0;
-        for (int j = 0; j < i; j++)
-            tempCdf += probabilities.at(j);
-
-        tempCdf += probabilities.at(i) / 2;
-        cdf.push_back(tempCdf);
-        codes.push_back("");
-    }
-
     /*
     Creates a dynamic array of structs for passing the data into the encoder
     each index holds a pointer to the probability and the cdf value
     */
-    arg_struct* args = new arg_struct[probabilities.size()];
-    for (int i = 0; i < probabilities.size(); i++)
-    {
-        args[i].prob = &probabilities.at(i);
-        args[i].cdf = &cdf.at(i);
-        args[i].code = &codes.at(i);
-    }
+    arg_struct args;
+    args.prob = &probabilities;
+    args.semB = &semB;
+    args.semP = &semP;
+    args.print = &print;
+    args.turn = &turn;
     
     // Creates all of the threads and passes in the data
     pthread_t tid[probabilities.size()];    
-    for (int i = 0; i < probabilities.size(); i++)
-        pthread_create(&tid[i], NULL, shannon, (void *) &args[i]);
+    for (int i = 0; i < probabilities.size(); i++) {
+        //critical section start
+        pthread_mutex_lock(&semB);  
+
+        args.thread_num = &i;
+        args.character = &characters.at(i);
+
+        pthread_create(&tid[i], NULL, shannon, &args); 
+    }
 
     // Threads are all rejoined
     for (int i = 0; i < probabilities.size(); i++)
         pthread_join(tid[i], NULL);
-    
+
+    /*
     // Output according to assignment formatting
     int i = 0;
     std::cout << "SHANNON-FANO-ELIAS Codes:" << std::endl << std::endl;
@@ -75,7 +82,7 @@ int main(int argc, char* argv[]) {
         std::cout << "Symbol " << key << ", Code: " << *args[i].code << std::endl;
         i++;
     }
-    
+    */
     return 0;
 }
 
@@ -85,31 +92,61 @@ shannon code into the overwrites the probability value so it can be accessed by 
 */
 void* shannon(void* arguments) {
 
-    std::string binary = "";
+    std::vector<double> cdf{};
+
     // The arguments come in as a void type so we must cast them back to the struct
     struct arg_struct* args = (arg_struct*)arguments;
 
-            // ----------------------------------------------------------------------------------------------
-            // Shannon encoding
-            // ----------------------------------------------------------------------------------------------
-            
-            // Shannon encoding length formula
-            int length = ceil(log2( 1 / *args->prob)) + 1;
+    int t_num = *args->thread_num - 1;
+    char symb = *args->character;
 
-            // Iterates through the cdf and if the fractional part is a 1 it adds a 1 to the code otherwise 0
-            while (length != 0) {
-                length--;
-                *args->cdf *= 2;
-                int fraction = *args->cdf;
+    // end the critical section within the child thread after copying the thread number into local variable in child thread
+    pthread_mutex_unlock(args->semB);
 
-                if (fraction == 1) {
-                    *args->cdf -= fraction;
-                    binary += '1';
-                } else {
-                    binary += '0';
-                }
-            }
+    // calculates the modified cdf value for each symbol and adds it to a vector
+    for (int i = 0; i < (*args->prob).size(); i++)
+    {
+        double tempCdf = 0.0;
+        for (int j = 0; j < i; j++)
+            tempCdf += (*args->prob).at(j);
 
-    *args->code = binary;
+        tempCdf += (*args->prob).at(i) / 2;
+        cdf.push_back(tempCdf);
+    }
+
+    // ----------------------------------------------------------------------------------------------
+    // Shannon encoding
+    // ----------------------------------------------------------------------------------------------
+    std::string binary = "";
+
+    // Shannon encoding length formula
+    int length = ceil(log2( 1 / (*args->prob).at(t_num))) + 1;
+
+    // Iterates through the cdf and if the fractional part is a 1 it adds a 1 to the code otherwise 0
+    while (length != 0) {
+        length--;
+        cdf.at(t_num) *= 2;
+        int fraction = cdf.at(t_num);
+
+        if (fraction == 1) {
+            cdf.at(t_num) -= fraction;
+            binary += '1';
+        } else {
+            binary += '0';
+        }
+    }
+
+    // synchronized printing
+    pthread_mutex_lock(args->semP);
+    while(*args->turn != t_num){
+        pthread_cond_wait(args->print, args->semP);
+    }
+
+    std::cout << "Symbol " << symb << ", " << "Code: " << binary << std::endl;
+
+    (*args->turn)++;
+    pthread_mutex_unlock(args->semP);
+    pthread_cond_broadcast(args->print);
+
     return NULL;
 }
